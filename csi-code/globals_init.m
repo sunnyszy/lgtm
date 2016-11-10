85d7b1fde23e4800a2015740612e609f4acb3e3e
sunnyszy@sjtu.edu.cn <87453371@qq.com>
2016-11-10 14:51:57 -0500

    auto computer subset antenna array

--- csi-code/globals_init.m
+++ csi-code/globals_init.m
@@ -1,9 +1,7 @@
 function globals_init
     %% DEBUG AND OUTPUT VARIABLES-----------------------------------------------------------------%%
     % Flow Controls
-    global AOA_EST_MODE
-    global SIMULATION
-    global SIMULAIION_ALWAYS_GENERATE_DATA
+    global AOA_EST_MODE SIMULATION SIMULAIION_ALWAYS_GENERATE_DATA
     AOA_EST_MODE = 'SPOTFI'; % 'MUSIC'  'SPOTFI'
     SIMULATION = true;
     SIMULAIION_ALWAYS_GENERATE_DATA = false;
@@ -21,17 +19,11 @@ function globals_init
     
     
     % Output controls
-    global OUTPUT_AOAS
-    global OUTPUT_TOFS
-    global OUTPUT_AOA_MUSIC_PEAK_GRAPH
-    global OUTPUT_TOF_MUSIC_PEAK_GRAPH
-    global OUTPUT_AOA_TOF_MUSIC_PEAK_GRAPH
-    global OUTPUT_SELECTIVE_AOA_TOF_MUSIC_PEAK_GRAPH
-    global OUTPUT_BINARY_AOA_TOF_MUSIC_PEAK_GRAPH
-    global OUTPUT_AOA_VS_TOF_PLOT
-    global OUTPUT_SUPPRESSED
-    global OUTPUT_PACKET_PROGRESS
-    global OUTPUT_FIGURES_SUPPRESSED
+    global OUTPUT_AOAS OUTPUT_TOFS OUTPUT_AOA_MUSIC_PEAK_GRAPH ...
+        OUTPUT_TOF_MUSIC_PEAK_GRAPH OUTPUT_AOA_TOF_MUSIC_PEAK_GRAPH ...
+        OUTPUT_SELECTIVE_AOA_TOF_MUSIC_PEAK_GRAPH OUTPUT_SUPPRESSED...
+        OUTPUT_BINARY_AOA_TOF_MUSIC_PEAK_GRAPH OUTPUT_AOA_VS_TOF_PLOT...
+        OUTPUT_PACKET_PROGRESS OUTPUT_FIGURES_SUPPRESSED 
     OUTPUT_AOAS = false;
     OUTPUT_TOFS = false;
     OUTPUT_AOA_MUSIC_PEAK_GRAPH = false;
@@ -44,17 +36,35 @@ function globals_init
     OUTPUT_PACKET_PROGRESS = false;
     OUTPUT_FIGURES_SUPPRESSED = false; % Set to true when running in deployment from command line
     
-    %% constant parameter
-    global d theta_the l_the channel_frequency delta_f n_subcarrier c n_antenna;
+    % constant parameter
+    global d theta_the l_the channel_frequency delta_f n_subcarrier c ...
+    n_antenna subset_x subset_y;
     theta_the = pi/6; % in rad
     l_the = 5; % in meter
     channel_frequency = 2462e6;
     delta_f = 312.5e3;
-    n_subcarrier = 56; % Atheros 56
+    n_subcarrier = 30; % Atheros 56
     c = 3e8; % speed of light
     n_antenna = 3;
-    
     d = c/channel_frequency/2; % distance between two antenna;
+    [subset_x, subset_y] = best_rank_subset(); %antenna subset used in smooth matrix
+    
+    function [sub_x, sub_y] = best_rank_subset()
+        score = zeros(n_subcarrier, n_antenna);
+        for x = 1:n_subcarrier-1
+            for y = 1:n_antenna-1
+                if x*y <= (n_antenna+1-y)*(n_subcarrier+1-x) % for the best of full rank
+                    score(x,y) = x*y;
+                else
+                    score(x,y) = 0;
+                end
+            end
+        end
+        ind = find(score==max(score(:)));
+        [sub_x, sub_y] = ind2sub([n_subcarrier, n_antenna],ind);
+    end
+    
+    
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 end
